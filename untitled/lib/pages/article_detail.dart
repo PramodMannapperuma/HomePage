8ff58a17429fbca86dc07520c863e922886183f8
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-06-11 11:00:01 +0530

    Updates news.dart

---
+++ untitled/lib/pages/article_detail.dart
@@ -0,0 +1,73 @@
+// // article_detail.dart
+// import 'package:flutter/material.dart';
+
+// class ArticleDetail extends StatelessWidget {
+//   final Map<String, String> article;
+
+//   const ArticleDetail({required this.article, Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(article['title']!),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Image.network(article['image']!),
+//             const SizedBox(height: 16),
+//             Text(
+//               article['title']!,
+//               style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//             const SizedBox(height: 16),
+//             Text(
+//               article['description']!,
+//               style: const TextStyle(fontSize: 18),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+class ArticleDetail extends StatelessWidget {
+  final Map<String, String> article;
+
+  const ArticleDetail({required this.article, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article['title']!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(article['image']!),
+            const SizedBox(height: 16),
+            Text(
+              article['title']!,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              article['description']!,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- untitled/lib/pages/news.dart
+++ untitled/lib/pages/news.dart
@@ -1,4 +1,134 @@
+// import 'package:flutter/material.dart';
+
+// class News extends StatefulWidget {
+//   const News({super.key});
+
+//   @override
+//   State<News> createState() => _NewsState();
+// }
+
+// class _NewsState extends State<News> {
+//   final List<Map<String, String>> newsArticles = [
+//     {
+//       'title': 'Today is monday!',
+//       'description': 'Date is June 10.',
+//       'image': 'https://www.vecteezy.com/free-vector/hello-june'
+//     },
+//     {
+//       'title': 'Happy Birthday',
+//       'description': 'Today is your birthday.',
+//       'image':
+//           'https://kentstreetcellars.com.au/products/happy-birthday-gift-card'
+//     },
+//     {
+//       'title': 'Tomorrow is Tuesday!',
+//       'description': 'Date is June 11.',
+//       'image': 'https://www.vecteezy.com/free-vector/hello-june'
+//     },
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('News'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: newsArticles.length,
+//         itemBuilder: (context, index) {
+//           final article = newsArticles[index];
+//           return Card(
+//             margin: const EdgeInsets.all(10),
+//             child: ListTile(
+//               leading: Image.network(article['image']!),
+//               title: Text(article['title']!),
+//               subtitle: Text(article['description']!),
+//               onTap: () {
+//                 // Handle news article tap
+//               },
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(const MaterialApp(
+//     home: News(),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// import 'article_detail.dart'; // Import the new file
+
+// class News extends StatefulWidget {
+//   const News({super.key});
+
+//   @override
+//   State<News> createState() => _NewsState();
+// }
+
+// class _NewsState extends State<News> {
+//   final List<Map<String, String>> newsArticles = [
+//     {
+//       'title': 'Today is monday!',
+//       'description': 'Date is June 10.',
+//       'image': 'https://www.vecteezy.com/free-vector/hello-june'
+//     },
+//     {
+//       'title': 'Happy Birthday',
+//       'description': 'Today is your birthday.',
+//       'image': 'https://kentstreetcellars.com.au/products/happy-birthday-gift-card'
+//     },
+//     {
+//       'title': 'Tomorrow is Tuesday!',
+//       'description': 'Date is June 11.',
+//       'image': 'https://www.vecteezy.com/free-vector/hello-june'
+//     },
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('News'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: newsArticles.length,
+//         itemBuilder: (context, index) {
+//           final article = newsArticles[index];
+//           return Card(
+//             margin: const EdgeInsets.all(10),
+//             child: ListTile(
+//               leading: Image.network(article['image']!),
+//               title: Text(article['title']!),
+//               subtitle: Text(article['description']!),
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => ArticleDetail(article: article),
+//                   ),
+//                 );
+//               },
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(const MaterialApp(
+//     home: News(),
+//   ));
+// }
+
 import 'package:flutter/material.dart';
+import 'article_detail.dart'; // Import the new file
 
 class News extends StatefulWidget {
   const News({super.key});
@@ -17,8 +147,7 @@ class _NewsState extends State<News> {
     {
       'title': 'Happy Birthday',
       'description': 'Today is your birthday.',
-      'image':
-          'https://kentstreetcellars.com.au/products/happy-birthday-gift-card'
+      'image': 'https://kentstreetcellars.com.au/products/happy-birthday-gift-card'
     },
     {
       'title': 'Tomorrow is Tuesday!',
@@ -44,7 +173,12 @@ class _NewsState extends State<News> {
               title: Text(article['title']!),
               subtitle: Text(article['description']!),
               onTap: () {
-                // Handle news article tap
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ArticleDetail(article: article),
+                  ),
+                );
               },
             ),
           );
@@ -54,10 +188,6 @@ class _NewsState extends State<News> {
   }
 }
 
-void main() {
-  runApp(const MaterialApp(
-    home: News(),
-  ));
-}
+
 
 
