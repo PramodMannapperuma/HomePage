86b670c1c257dab67b433e31c2020e2f8f05cf46
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-06-24 15:01:46 +0530

    updated

--- untitled/lib/pages/news_screen.dart
+++ untitled/lib/pages/news_screen.dart
@@ -341,7 +341,7 @@ class NewsScreen extends StatelessWidget {
       elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Container(
-        width: 340,
+        width: 365,
         padding: const EdgeInsets.all(10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
