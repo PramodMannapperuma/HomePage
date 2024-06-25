3c7fc95199726ff789afa99dfeda131b53d910bd
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-06-25 10:34:50 +0530

    Update page to use the 'event' widget instead of 'News'

--- untitled/lib/pages/news_screen.dart
+++ untitled/lib/pages/news_screen.dart
@@ -227,7 +227,7 @@ class NewsScreen extends StatelessWidget {
                   context,
                   title: 'Event',
                   icon: Icons.event,
-                  page: const News(),
+                  page: const event(),
                 ),
               ],
             ),
