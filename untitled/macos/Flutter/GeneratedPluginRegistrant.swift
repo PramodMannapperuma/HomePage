2db931b618a5532325b9878ac4b47561b81e6e9a
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2024-07-05 12:08:19 +0530

    minor changes

--- untitled/macos/Flutter/GeneratedPluginRegistrant.swift
+++ untitled/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import device_info_plus
 import file_selector_macos
-import location
 import pdfx
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
 }
