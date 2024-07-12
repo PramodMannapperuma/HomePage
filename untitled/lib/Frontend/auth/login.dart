9847960a977087a8ca61b0eba38f6a050543e4b4
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-07-12 12:43:28 +0530

    Changing login UI

--- untitled/lib/Frontend/auth/login.dart
+++ untitled/lib/Frontend/auth/login.dart
@@ -14,40 +14,45 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-
     var mediaQuery = MediaQuery.of(context);
     var screenWidth = mediaQuery.size.width;
     var screenHeight = mediaQuery.size.height;
-    var isTabletOrLarger = screenWidth >= 600;
 
     return Scaffold(
       appBar: customAppBar(
           title: '', showActions: false, showLeading: false, context: context),
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 0,
+          Container(
+            width: double.infinity,
+            height: screenHeight * 0.3, // Adjust image height
             child: Image.asset(
-              'assets/images/hrislogo2.png', // Replace with your image path
-              width: screenWidth * 0.9, // Make the image width responsive
-              height: screenHeight * 0.2, // Make the image height responsive
+              'assets/images/test-bg.png', // Replace with your image path
+              fit: BoxFit.cover,
             ),
           ),
           Expanded(
-            child: Center(
+            child: Align(
+              alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: screenHeight *0.135),
-                    Padding(
-                      padding: EdgeInsets.all(1.0),
-                      child: LoginForm(),
-                    ),
-                    SizedBox(height: 10.0),
-                  ],
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(77, 40, 128, 0.2), // Updated background color
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                        offset: Offset(0, 3), // Shadow position
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: LoginForm(),
+                  ),
                 ),
               ),
             ),
@@ -82,8 +87,7 @@ class _LoginFormState extends State<LoginForm> {
 
     try {
       showDialog(
-        barrierDismissible:
-            false, // Prevents the dialog from closing until we manually do it
+        barrierDismissible: false, // Prevents the dialog from closing until we manually do it
         context: context,
         builder: (BuildContext context) {
           return const AlertDialog(
@@ -165,26 +169,41 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        TextField(
-          controller: _usernameController,
-          decoration: InputDecoration(
-            labelText: 'Username',
-            border: UnderlineInputBorder(),
+        Container(
+          margin: const EdgeInsets.only(bottom: 15.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.grey.withOpacity(0.5)),
+          ),
+          child: TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            ),
           ),
         ),
-        SizedBox(height: 10.0),
-        TextField(
-          controller: _passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            labelText: 'Password',
-            border: UnderlineInputBorder(),
+        Container(
+          margin: const EdgeInsets.only(bottom: 15.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.grey.withOpacity(0.5)),
+          ),
+          child: TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            ),
           ),
         ),
-        SizedBox(height: 10.0),
         GestureDetector(
           onTap: () {
             // Add your forgot password logic here
@@ -192,24 +211,23 @@ class _LoginFormState extends State<LoginForm> {
           },
           child: Text(
             'Forgot Password?',
+            textAlign: TextAlign.right,
             style: TextStyle(
               color: Colors.blue,
               fontSize: 16.0,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-        SizedBox(height: 10.0),
+        SizedBox(height: 20.0),
         ElevatedButton(
           onPressed: _login,
           style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF4d2880),
-            padding: EdgeInsets.symmetric(
-              vertical: 10.0,
-            ),
-            minimumSize: Size(double.infinity, 50.0),
+            primary: Colors.deepPurple, // Gradient background
+            padding: EdgeInsets.symmetric(vertical: 15.0),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10.0),
-              side: BorderSide(color: Color(0xFF4d2880)),
+              side: BorderSide(color: Colors.deepPurple),
             ),
           ),
           child: Text(
@@ -228,3 +246,236 @@ class _LoginFormState extends State<LoginForm> {
     super.dispose();
   }
 }
+
+
+
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import '../app_bar.dart';
+// import '../home/Dashbord.dart';
+
+// class Login extends StatefulWidget {
+//   const Login({super.key});
+
+//   @override
+//   State<Login> createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+//   @override
+//   Widget build(BuildContext context) {
+
+//     var mediaQuery = MediaQuery.of(context);
+//     var screenWidth = mediaQuery.size.width;
+//     var screenHeight = mediaQuery.size.height;
+//     var isTabletOrLarger = screenWidth >= 600;
+
+//     return Scaffold(
+//       appBar: customAppBar(
+//           title: '', showActions: false, showLeading: false, context: context),
+//       body: Stack(
+//         children: [
+//           Positioned(
+//             top: 0,
+//             child: Image.asset(
+//               'assets/images/hrislogo2.png', // Replace with your image path
+//               width: screenWidth * 0.9, // Make the image width responsive
+//               height: screenHeight * 0.2, // Make the image height responsive
+//             ),
+//           ),
+//           Expanded(
+//             child: Center(
+//               child: Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     SizedBox(height: screenHeight *0.135),
+//                     Padding(
+//                       padding: EdgeInsets.all(1.0),
+//                       child: LoginForm(),
+//                     ),
+//                     SizedBox(height: 10.0),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class LoginForm extends StatefulWidget {
+//   @override
+//   _LoginFormState createState() => _LoginFormState();
+// }
+
+// class _LoginFormState extends State<LoginForm> {
+//   final _usernameController = TextEditingController();
+//   final _passwordController = TextEditingController();
+//   bool _isLoading = false;
+
+//   Future<void> _login() async {
+//     String username = _usernameController.text;
+//     String password = _passwordController.text;
+
+//     if (username.isEmpty || password.isEmpty) {
+//       _showErrorDialog('Username and password cannot be empty');
+//       return;
+//     }
+//     setState(() {
+//       _isLoading = true;
+//     });
+
+//     try {
+//       showDialog(
+//         barrierDismissible:
+//             false, // Prevents the dialog from closing until we manually do it
+//         context: context,
+//         builder: (BuildContext context) {
+//           return const AlertDialog(
+//             content: Row(
+//               children: [
+//                 CircularProgressIndicator(),
+//                 SizedBox(width: 24),
+//                 Text("Logging In ... "),
+//               ],
+//             ),
+//           );
+//         },
+//       );
+
+//       final response = await http.post(
+//         Uri.parse('http://hris.accelution.lk/api/auth'),
+//         headers: <String, String>{
+//           'Content-Type': 'application/json; charset=UTF-8',
+//         },
+//         body: jsonEncode(<String, String>{
+//           'client_id': username,
+//           'client_secret': password,
+//         }),
+//       );
+//       Navigator.pop(context);
+//       setState(() {
+//         _isLoading = false;
+//       });
+
+//       if (response.statusCode == 200) {
+//         final data = json.decode(response.body);
+//         final token = data['access_token'];
+//         print('Token In login $token');
+
+//         // Assuming the token is obtained correctly
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(
+//             builder: (context) => MainScreen(token: token),
+//           ),
+//         );
+
+//       } else {
+//         final errorData = json.decode(response.body);
+//         _showErrorDialog(
+//             errorData['message'] ?? 'Invalid username or password');
+//         // Navigator.pop(context);
+//       }
+//     } catch (e) {
+//       setState(() {
+//         _isLoading = false;
+//       });
+//       _showErrorDialog('An error occurred. Please try again. $e');
+//       print('Login error: $e'); // Logging for debugging
+//       // Navigator.pop(context);
+//     }
+//   }
+
+//   void _showErrorDialog(String message) {
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: Text('Login Failed'),
+//           content: Text(message),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//               child: Text('OK'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         TextField(
+//           controller: _usernameController,
+//           decoration: InputDecoration(
+//             labelText: 'Username',
+//             border: UnderlineInputBorder(),
+//           ),
+//         ),
+//         SizedBox(height: 10.0),
+//         TextField(
+//           controller: _passwordController,
+//           obscureText: true,
+//           decoration: InputDecoration(
+//             labelText: 'Password',
+//             border: UnderlineInputBorder(),
+//           ),
+//         ),
+//         SizedBox(height: 10.0),
+//         GestureDetector(
+//           onTap: () {
+//             // Add your forgot password logic here
+//             print('Forgot Password tapped');
+//           },
+//           child: Text(
+//             'Forgot Password?',
+//             style: TextStyle(
+//               color: Colors.blue,
+//               fontSize: 16.0,
+//             ),
+//           ),
+//         ),
+//         SizedBox(height: 10.0),
+//         ElevatedButton(
+//           onPressed: _login,
+//           style: ElevatedButton.styleFrom(
+//             backgroundColor: Color(0xFF4d2880),
+//             padding: EdgeInsets.symmetric(
+//               vertical: 10.0,
+//             ),
+//             minimumSize: Size(double.infinity, 50.0),
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(10.0),
+//               side: BorderSide(color: Color(0xFF4d2880)),
+//             ),
+//           ),
+//           child: Text(
+//             'Login',
+//             style: TextStyle(fontSize: 18.0, color: Colors.white),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _usernameController.dispose();
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+// }
