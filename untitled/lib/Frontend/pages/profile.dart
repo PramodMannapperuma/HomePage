868afce633efc53642082cc5613e480aac725929
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-07-23 12:29:05 +0530

    Update profile.dart

--- untitled/lib/Frontend/pages/profile.dart
+++ untitled/lib/Frontend/pages/profile.dart
@@ -114,7 +114,7 @@ class _ProfilePageState extends State<ProfilePage> {
                                 color: Color(0xff4d2880),
                               ),
                             ),
-                            SizedBox(height: 4),
+                            SizedBox(height: 0.2),
                             Text(
                               designation,
                               style: TextStyle(
