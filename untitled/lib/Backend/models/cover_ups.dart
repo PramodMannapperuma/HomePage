63a5b406d5d743261d4f8a9502447fccd030b95f
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2024-07-27 08:31:45 +0530

    completed the Coverups API, Leave types API and leave API

---
+++ untitled/lib/Backend/models/cover_ups.dart
@@ -0,0 +1,17 @@
+class CoverUp {
+  final String id;
+  final String name;
+
+  CoverUp({required this.id, required this.name});
+
+  factory CoverUp.fromJson(Map<String, dynamic> json) {
+    return CoverUp(
+      id: json['value'],
+      name: json['text'],
+    );
+  }
+
+  static List<CoverUp> fromJsonList(List<dynamic> jsonList) {
+    return jsonList.map((json) => CoverUp.fromJson(json as Map<String, dynamic>)).toList();
+  }
+}
---
+++ untitled/lib/Backend/models/leave_types.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class LeaveType {
+  final int value;
+  final String text;
+  final Map<String, dynamic> additionalData;
+
+  LeaveType({
+    required this.value,
+    required this.text,
+    required this.additionalData,
+  });
+
+  factory LeaveType.fromJson(Map<String, dynamic> json) {
+    return LeaveType(
+      value: json['value'],
+      text: json['text'],
+      // Decode the data if it's a string
+      additionalData: json['data'] is String
+          ? jsonDecode(json['data']) as Map<String, dynamic>
+          : json['data'] as Map<String, dynamic>,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'value': value,
+      'text': text,
+      'data': jsonEncode(additionalData),
+    };
+  }
+
+  // Convert a list of JSON maps into a list of LeaveType objects
+  static List<LeaveType> fromJsonList(List<dynamic> jsonList) {
+    return jsonList.map((json) => LeaveType.fromJson(json as Map<String, dynamic>)).toList();
+  }
+
+  // Convert a list of LeaveType objects into a list of JSON maps
+  static List<Map<String, dynamic>> toJsonList(List<LeaveType> leaveTypes) {
+    return leaveTypes.map((leaveType) => leaveType.toJson()).toList();
+  }
+}
