2ac3660f1536a36a480391ec9dae72cce5c81489
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-08-02 10:45:57 +0530

    Update career_profile.dart

--- untitled/lib/Frontend/profile/career_profile.dart
+++ untitled/lib/Frontend/profile/career_profile.dart
@@ -63,21 +63,20 @@ class _CareerProfileState extends State<CareerProfile> {
                       if (snapshot.connectionState ==
                           ConnectionState.waiting) {
                         return CircularProgressIndicator();
-                      } else if (snapshot.hasError) {
+                      }  else if (snapshot.hasError) {
                         return CircleAvatar(
-                            radius: 60,
-                            child: Center(
-                                child: Text('Error: ${snapshot.error}', style: TextStyle(fontSize: 8),)));
+                          radius: 45,
+                          backgroundImage: AssetImage('assets/images/profile.png'),
+                        );
                       } else if (snapshot.hasData) {
                         return CircleAvatar(
-                            radius: 60,
-                            backgroundImage: MemoryImage(snapshot.data!));
+                          radius: 45,
+                          backgroundImage: MemoryImage(snapshot.data!),
+                        );
                       } else {
-
                         return CircleAvatar(
-                          radius: 60,
-                          backgroundColor: Colors.black45,
-                          child: Icon(Icons.person_rounded, color: Colors.white,),
+                          radius: 45,
+                          backgroundImage: AssetImage('assets/images/profile.png'),
                         );
                       }
                     },
