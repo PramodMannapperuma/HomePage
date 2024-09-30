5d7a9c73867ff9036d8b87ad7ced3adead69e6a3
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-09-30 14:42:39 +0530

    Update login.dart

--- untitled/lib/Frontend/auth/login.dart
+++ untitled/lib/Frontend/auth/login.dart
@@ -42,8 +42,8 @@ class _LoginState extends State<Login> {
               Image.asset(
                 'assets/images/test-bg.png',
                 height: screenHeight * 0.4, // Adjust image height
-                width: screenWidth * 0.9,
-                fit: BoxFit.fill,
+                width: screenWidth * 0.8,
+                fit: BoxFit.contain
               ),
               LoginForm(
                 textFieldHeight: textFieldHeight,
