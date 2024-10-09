3c86c570b941445d8b0afdbd3c2d7d3e6bd7392e
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-10-09 12:34:16 +0530

    Update pending approvals

--- untitled/lib/Backend/models/approval_items.dart
+++ untitled/lib/Backend/models/approval_items.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 class ApprovalItem {
+  final int id;
   final String item;
   final String employee;
   final String designation;
   final String code;
 
   ApprovalItem({
+    required this.id,
     required this.item,
     required this.employee,
     required this.designation,
@@ -15,6 +17,7 @@ class ApprovalItem {
 
   factory ApprovalItem.fromJson(Map<String, dynamic> json) {
     return ApprovalItem(
+      id: json['id'] as int,
       item: json['item'] as String,
       employee: json['employee'] as String,
       designation: json['designation'] as String,
